16607030de76f3e03c76d3b6585b9e9cfe6c584a
MathijsBernsen <mathijsbernsen@gmail.com>
2024-12-30 13:15:10 +0100

    Added song seeder and factory settings

---
+++ database/factories/SongFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Song>
+ */
+class SongFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'duration' => $this->faker->randomNumber(2),
+            'album_id' => \App\Models\Album::factory(),
+            'release_date' => $this->faker->date(),
+        ];
+    }
+}
